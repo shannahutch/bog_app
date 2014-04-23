0066095e305ed3c67f6712efc9ff0863f9b7e023
Githubshanna <shannahutch@gmail.com>
2014-04-22 18:10:28 -0700

    sending the creatures

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,17 @@
+class CreaturesController < ApplicationController
+
+	def index
+		@creatures = Creature.all
+		render :index
+	end
+
+	def new
+		render :new
+	end
+
+	def create
+		creature = params.require(:creatures).permit(:name, :kind, :description)
+		Creature.create(creature)
+		redirect_to "/creatures"
+	end
+end
---
+++ app/models/creature.rb
@@ -0,0 +1,3 @@
+class Creature < ActiveRecord::Base
+	
+end
